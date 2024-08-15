04bda8fe1c32aa4754490c02cbed85a5e8ea341f
sthefany-silva <maynarasthefany695@gmail.com>
2024-08-15 12:53:58 -0300

    repetições em javascript

---
+++ exercicios/aula009/ambiente.js
@@ -0,0 +1,16 @@
+/*var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <=6)*/
+
+/*var c = 1
+while (c <= 3){
+    console.log(`passo ${c}`)
+    c++
+}*/
+console.log('vai começar!')
+for (var c=1; c<=5; c++){
+    console.log(`passo ${c}`)
+}
+console.log('fim.')
