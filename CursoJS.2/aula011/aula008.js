24c05e92364a8e324399332e36e6ec8c7d1aa5ea
sthefany-silva <maynarasthefany695@gmail.com>
2024-12-02 16:35:21 -0300

    exercicios

---
+++ CursoJS.2/aula011/aula008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}else{
+    console.log('dirija sempre usando cinto de segurança')
+}
+
---
+++ CursoJS.2/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+if(país == 'BRASIL'){
+    console.log('brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
