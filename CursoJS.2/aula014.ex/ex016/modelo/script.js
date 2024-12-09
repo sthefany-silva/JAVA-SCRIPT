5a232fb3b869c9628b278078fb4057959cf78923
sthefany-silva <maynarasthefany695@gmail.com>
2024-12-09 17:20:50 -0300

    exercicios de repetição

---
+++ CursoJS.2/aula014.ex/ex016/modelo/script.js
@@ -0,0 +1,32 @@
+ function contar(){
+ let inicio = document.getElementById('txti')
+ let fim = document.getElementById('txtf')
+ let passo = document.getElementById('txtp')
+ let res = document.getElementById('res')
+
+ if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    
+    res.innerHTML = 'Impossível contar!'
+ } else {
+    res.innerHTML = 'contando: <br>'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if(p <= 0){
+        window.alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+    }
+    if (i < f){
+        for (let c = 1; c <= f; c += p){
+             res.innerHTML += `${c} \u{1F449}`
+        }
+} else {
+        for (let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+                
+            }
+        } 
+        res.innerHTML += `\u{1F3C1}`
+    }
+
+ }
